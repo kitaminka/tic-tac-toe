0259fd56a7f6312338f1e4cfa15d56ff12b444d8
Kitaminka <kit4minka@gmail.com>
2021-03-02 09:49:24 +0200

    Fixed authorization error

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,23 +3,31 @@ const fetch = require('node-fetch');
 module.exports = {
     async authUser(req, res) {
         if (!req.query.code) return res.status(403).send('Forbidden');
+        const uri = 'http://127.0.0.1/users/auth/';
 
         const params = new URLSearchParams();
         params.append('client_id', '815550306351710238');
         params.append('grant_type', 'authorization_code');
         params.append('client_secret', 'zgpuHBkzdQRhsaOn0oulXIFJvw7YoU1l');
         params.append('code', req.query.code);
-        params.append('redirect_uri', 'https://google.com');
-        params.append('scope', 'identify email connections');
+        params.append('redirect_uri', uri);
 
-        const data = await fetch('https://discord.com/api/oauth2/token', {
+        const data = await fetch('https://discord.com/api/v8/oauth2/token', {
             method: 'POST',
             body: params,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         }).then((res) => res.json());
-        console.log(data);
+
+        const user = await fetch(`https://discord.com/api/v8/users/@me`, {
+            method: 'GET',
+            headers: {
+                'Authorization':`${data.token_type} ${data.access_token}`
+            }
+        }).then((res) => res.json());
+        console.log(user);
+        res.redirect('/');
     },
     getUsers(req, res) {
         res.send('Список пользователей');
