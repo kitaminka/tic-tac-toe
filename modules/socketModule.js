2a138e7ae2473d8c496d70f8251c950746f6f234
Nikita <kit4minka@gmail.com>
2021-05-31 22:23:46 +0300

    Update room sockets and add some TODOs

--- modules/socketModule.js
+++ modules/socketModule.js
@@ -7,22 +7,41 @@ let gameState = 0;
 
 module.exports = (io) => {
     io.sockets.on('connection', (socket) => {
+        console.log(`--${socket.id}`) // Remove this
         if (connections.length === 2) socket.disconnect;
         connections.push(socket);
         if (connections.length === 2) gameState = 1;
-        if (gameState === 1) io.emit('gameStart', [
-            {
-                socketId: connections[0].id,
-                turn: 1
-            },
-            {
-                socketId: connections[1].id,
-                turn: 2
+        if (gameState === 1) {
+            io.emit('gameStart', [
+                {
+                    socketId: connections[0].id,
+                    turn: 1
+                },
+                {
+                    socketId: connections[1].id,
+                    turn: 2
+                }
+            ]);
+            io.emit('firstTurn');
+            gameState = 2;
+        }
+        socket.on('turn', (data) => {
+            io.emit('turnInfo', {
+                socketId: socket.id,
+                turn: data
+            });
+            if (gameState === 1) {
+                io.emit('firstTurn');
+                gameState = 2;
+            } else {
+                io.emit('secondTurn');
+                gameState = 1;
             }
-        ]);
-        io.emit('firstTurn');
-    });
-    io.sockets.on('disconnect', () => {
-        for (const socket of connections) socket.disconnect;
+        });
+        socket.on('disconnect', () => {
+            connections.splice(connections.indexOf(socket), 1);
+            // TODO Add room removing and user updating
+        });
+        // TODO Add win
     });
 }
--- public/js/room.js
+++ public/js/room.js
@@ -1,4 +1,6 @@
 'use strict'
+let turn;
+
 document.addEventListener("DOMContentLoaded",async () => {
     const roomId = window.location.href.split('/')[window.location.href.split('/').length - 1];
     const result = await fetch(`/rooms/${roomId}`, {
@@ -8,17 +10,23 @@ document.addEventListener("DOMContentLoaded",async () => {
         window.location.href = 'http://127.0.0.1/game';
     } else {
         const socket = io.connect();
-        let turn;
-
         socket.on('connect', () => {
             console.log(socket.id)
         });
         socket.on('gameStart', (data) => {
+            console.log('gameStart!');
+            console.log(data)
            if (data[0].socketId === socket.id) turn = data[0].turn;
            else if (data[1].socketId === socket.id) turn = data[1].turn;
-           // TODO Add eventListener
+           document.addEventListener('click', (event) => {
+                if (event.target.className === 'button') {
+                    socket.emit('turn', event.target.innerHTML);
+                }
+            });
         });
         socket.on('firstTurn', () => {
+            console.log('firstTurn!')
+            console.log(turn)
             if (turn === 1) {
                 const buttons = document.getElementsByClassName('button');
                 for (const button of buttons) button.removeAttribute('disabled');
@@ -28,6 +36,7 @@ document.addEventListener("DOMContentLoaded",async () => {
             }
         });
         socket.on('secondTurn', () => {
+            console.log('secondTurn!')
             if (turn === 2) {
                 const buttons = document.getElementsByClassName('button');
                 for (const button of buttons) button.removeAttribute('disabled');
@@ -36,5 +45,21 @@ document.addEventListener("DOMContentLoaded",async () => {
                 for (const button of buttons) button.setAttribute('disabled', 'true');
             }
         });
+        socket.on('turnInfo', (data) => {
+            // TODO Fix class names and ids
+            const element = document.getElementById(data.turn);
+            if (data.socketId === socket.id) {
+                element.innerHTML = 'X';
+            } else {
+                element.innerHTML = 'O';
+            }
+            element.classList.remove('button');
+            element.setAttribute('disabled', 'true');
+        });
+        socket.on('disconnect', () => {
+            console.log('disconnect!');
+            // TODO Redirect to main page
+        })
     }
 });
+// Remove all debug console.log after finishing
